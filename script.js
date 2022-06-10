9f5be7d74f1570c1cf7e7ecce3d9281f22b76501
dwiky11 <dwiky.s98@gmail.com>
2022-06-10 17:38:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('.menu input');
+const nav = document.querySelector('nav ul');
+const body = document.querySelector("body");
+const toggle = document.querySelector("#toggle");
+const sunIcon = document.querySelector(".toggle .bxs-sun");
+const moonIcon = document.querySelector(".toggle .bx-moon");
+
+
+
+menu.addEventListener('click',() => {
+    nav.classList.toggle('slide');
+});
+
+
+toggle.addEventListener("change", () => {
+    
+    body.classList.toggle("dark");
+    sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+    moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+
+});
